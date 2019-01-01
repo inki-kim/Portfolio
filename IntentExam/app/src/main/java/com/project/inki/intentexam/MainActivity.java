55d91f5e954ce9204fad45e2cf446adbe9ed95ec
inki-kim <aid93@naver.com>
2019-01-01 21:30:59 +0900

    IntentExample

--- IntentExam/app/src/main/java/com/project/inki/intentexam/MainActivity.java
+++ IntentExam/app/src/main/java/com/project/inki/intentexam/MainActivity.java
@@ -1,8 +1,11 @@
 package com.project.inki.intentexam;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +13,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
                 Button button2 = (Button) linearLayout.findViewById(R.id.finish);
                 Button button1 = (Button) linearLayout.findViewById(R.id.newActivity);
+                Button openButton=(Button) linearLayout.findViewById(R.id.openButton);
+
+                final EditText inputPdfName=(EditText) linearLayout.findViewById(R.id.inputPdfName);
 
                 button1.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -56,6 +65,21 @@ public class MainActivity extends AppCompatActivity {
                         finish();
                     }
                 });
+
+                openButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String filename = inputPdfName.getText().toString();    //PDF파일명을 가져와 filename에 저장
+
+                        if (filename.length() > 0) {                          //filename에 어떤것이 적혀져있을경우 openPdf라는 메소드에 파일명을 파라메터로 전달하며 호출
+                            openPdf(filename.trim());
+                        }
+                        else{                                                 //다시입력
+                            Toast.makeText(getApplicationContext(),"pdf파일명을 다시 입력하세요.",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
             }
         });
     }
@@ -80,4 +104,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void openPdf(String filename){                          //pdf를 열기위한 메소드
+        String sdcardFolder=Environment.getExternalStorageDirectory().getAbsolutePath();         //내장메모리 파일의 절대경로 지정
+        String filepath=sdcardFolder+ File.separator+filename;                                  //원하는 파일을 열기위한 "/" 추가와 파일명 추가작업
+        Toast.makeText(getApplicationContext(),filepath,Toast.LENGTH_LONG).show();
+        File file=new File(filepath);                           //파일의 경로로 파일객체를 생성
+
+        if(file.exists()){                                      //파일이 존재할경우
+            Uri uri = Uri.fromFile(file);     //Uri 객체로 생성
+
+            Intent intent=new Intent(Intent.ACTION_VIEW);              //액션을 지정한 인텐트 생성
+            intent.setDataAndType(uri,"application/pdf");       //uri객체와 MIME 타입 지정
+
+            try {
+                startActivity(intent);
+            }catch(ActivityNotFoundException ex){
+                Toast.makeText(getApplicationContext(),"pdf파일을 보기위한 뷰어가 없습니다.",Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"pdf파일이 없습니다.",Toast.LENGTH_LONG).show();
+        }
+    }
 }
