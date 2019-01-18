35cb09091d2f0f453d63435346105f68741bd3ee
inki-kim <aid93@naver.com>
2019-01-18 20:17:36 +0900

    Calendar Example

---
+++ CalendarExam/app/src/main/java/com/project/inki/calendarexam/MonthItem.java
@@ -0,0 +1,15 @@
+package com.project.inki.calendarexam;
+
+
+public class MonthItem {
+    private int dayValue;
+
+    public MonthItem(int day) {
+        dayValue = day;
+    }
+
+    public int getDay() {
+        return dayValue;
+    }
+
+}
